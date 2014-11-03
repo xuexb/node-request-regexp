2ea4bd8e84e5f02412b99117b2e1ab9754c4de28
xiaowu <admin@xuexb.com>
2014-11-03 16:54:52 +0800

    添加文件

---
+++ request-regexp.js
@@ -0,0 +1,68 @@
+var request = require('request');
+var iconv = require('iconv-lite');
+
+module.exports = function(options, callback) {
+    'use strict';
+
+    var reg,
+        is_gb2312 = false;
+
+    if ('string' === typeof options) {
+        options = {
+            url: options
+        }
+    }
+
+    // charset is BG2312
+    if (options.encoding === 'gb2312') {
+        options.encoding = null;
+        is_gb2312 = true;
+    }
+
+    //RegExp
+    reg = options.regexp;
+
+    callback = callback || function() {};
+
+
+    return request(options, function(error, response, body) {
+        var result = {},
+            key, temp;
+
+        //Transcoding
+        if (is_gb2312) {
+            body = iconv.decode(body, 'gb2312').toString();
+        }
+
+        //no RegExp
+        if (!reg) {
+            return callback(error, response, body);
+        }
+
+        if (error) {
+            callback(error, response, result);
+        } else {
+            if (response.statusCode === 200) {
+                temp = {};
+
+                //Global search
+                Object.keys(reg).forEach(function(value) {
+                    temp[value] = body.match(new RegExp(reg[value], 'g')) || [];
+                });
+
+                //Find every and additional
+                Object.keys(temp).forEach(function(value) {
+                    result[value] = [];
+                    temp[value].forEach(function(str) {
+                        result[value].push((str.match(new RegExp(reg[value])) || ['']).slice(1));
+                    });
+                });
+
+                callback(error, response, result);
+            } else {
+                callback(error, response, result);
+            }
+        }
+
+    });
+}
---
+++ test.js
@@ -0,0 +1,17 @@
+var request = require('./request-regexp');
+
+request({
+    url: 'http://www.qq.com/',
+    encoding: 'gb2312',
+    regexp: {
+        title: '<title>(.+?)</title>',
+        keyword: '<meta content="(.+?)" name="Keywords">',
+        description: '<meta name="description" content="(.+?)" />'
+    }
+}, function(error, response, body) {
+    if (error) {
+        console(error.message);
+    } else {
+        console.log(body);
+    }
+});
